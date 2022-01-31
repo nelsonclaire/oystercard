077d1ee1ba284668f5819cbb0022681f91adf411
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-01-31 17:31:12 +0000

    Add journey and touch in, touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,30 @@
 class Oystercard
 
-    attr_reader :balance
-
+    attr_reader :balance, :in_journey
+    MAX_BALANCE = 90
     def initialize
         @balance = 0
+        @in_journey = false
     end
 
     def top_up(value)
+      fail 'Maximum balance of #{max_balance} exceeded' unless value + balance <= MAX_BALANCE
       @balance += value
     end
 
+    def deduct(value)
+      @balance -= value
+    end
+
+    def in_journey?
+      @in_journey
+    end
+
+    def touch_in
+      @in_journey = true
+    end
+
+    def touch_out
+      @in_journey = false
+    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,7 +10,40 @@ describe Oystercard do
       it { is_expected.to respond_to(:top_up).with(1).argument }
 
       it 'should top up balance by value' do
-        expect { subject.top_up 2 }.to change { subject.balance }.by(2)
+        expect { subject.top_up 2 }.to change { subject.balance }.by 2
+      end
+
+      it 'should raise an error if maximum balance exceeded' do
+        max_balance = Oystercard::MAX_BALANCE
+        subject.top_up(max_balance)
+        expect { subject.top_up 1 }.to raise_error('Maximum balance of #{max_balance} exceeded')
+      end
+    end
+
+    describe '#deduct' do
+      it { is_expected.to respond_to(:deduct).with(1).argument }
+
+      it 'should deduct balance by value' do
+        subject.top_up(10)
+        expect { subject.deduct 2 }.to change { subject.balance }.by -2
+      end
+
+    end
+
+    describe '#in_journey?' do
+      it 'is not on a journey' do
+        expect(subject).not_to be_in_journey
+      end
+
+      it 'can touch in' do
+        subject.touch_in
+        expect(subject).to be_in_journey
+      end
+
+      it 'can touch out' do
+        subject.touch_in
+        subject.touch_out
+        expect(subject).not_to be_in_journey
       end
     end
 
