c16bcddc5932d8937563db5d978be0c572fef9d5
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-01-31 15:35:20 +0000

    add debugging basics for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
+require 'oystercard.rb'
 
+describe Oystercard do
+    
+    it 'should have a balance of zero' do
+        expect(subject.balance).to eq(0)
+    end
 
-decribe Oystercard do
-  
 end
