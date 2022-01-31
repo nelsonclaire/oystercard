b3a8b6f297f285b296034e97899d998f43fac7ee
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-01-31 16:10:59 +0000

    add top_up functionality

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,12 @@ describe Oystercard do
         expect(subject.balance).to eq(0)
     end
 
+    describe '#top_up' do
+      it { is_expected.to respond_to(:top_up).with(1).argument }
+
+      it 'should top up balance by value' do
+        expect { subject.top_up 2 }.to change { subject.balance }.by(2)
+      end
+    end
+
 end
